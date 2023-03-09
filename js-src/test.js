36439f7c61fbe3a279d7fbea62db582806bbac4b
Johan Brichau <johan@yesplan.be>
2023-03-09 21:27:29 +0100

    interesting js

---
+++ js-src/test.js
@@ -0,0 +1,14 @@
+true == []; // -> false
+true == ![]; // -> false
+
+toNumber(true); // -> 1
+toNumber([]); // -> 0
+
+1 == 0; // -> false
+true == ![]; // -> false
+
+![]; // -> false
+
+true == false; // -> false
+
+"b" + "a" + +"a" + "a"; // -> 'baNaNa'
